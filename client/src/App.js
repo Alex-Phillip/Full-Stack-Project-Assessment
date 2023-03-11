861b916f6929c0b3869ca63f3604727a557bff4e
Alex-Phillip <alexjoelphillip@gmail.com>
2023-03-11 19:29:50 +0000

    css reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
+import './App.css'
+import videoData from './exampleresponse.json'
 
 function App() {
+  console.log(videoData)
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
